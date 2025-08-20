a2bb450b75503ee6685c3c3610397164c37e2708
Elwaliid <walidboubaidja@gmail.com>
2025-08-20 18:00:56 +0100

    Update login_screen.dart

--- crm_frontend/lib/view/Screens/login_screen.dart
+++ crm_frontend/lib/view/Screens/login_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
