599913ff60cd9c0d5082770d1772bf600d3780c3
Elwaliid <walidboubaidja@gmail.com>
2025-08-26 22:27:31 +0100

    3

--- crm_backend/routes/user_router.js
+++ crm_backend/routes/user_router.js
@@ -4,7 +4,7 @@ const UserController = require("../controller/user_controller");
 router.post('/registeration', UserController.register);
 router.post('/login', UserController.login);
 
-router.post("/google-login", UserController.googleLogin);
-router.post("/apple-login", UserController.appleLogin);
+router.post("/googleLogin", UserController.googleLogin);
+router.post("/appleLogin", UserController.appleLogin);
 
 module.exports = router;
--- crm_frontend/lib/config.dart
+++ crm_frontend/lib/config.dart
@@ -1,5 +1,5 @@
 final baseUrl = "http://localhost:3000";
 final registerUrl = "$baseUrl/registeration";
 final loginUrl = "$baseUrl/login";
-final GloginUrl = "$baseUrl/google-login";
-final AloginUrl = "$baseUrl/apple-login";
+final GloginUrl = "$baseUrl/googleLogin";
+final AloginUrl = "$baseUrl/appleLogin";
