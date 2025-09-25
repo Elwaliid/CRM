8d19b21f0247abc2581e45e2a5dd10d81a49074d
Elwaliid <walidboubaidja@gmail.com>
2025-09-25 16:51:51 +0100

    added task,client modules

---
+++ crm_backend/models/client_model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const db = require('../config/db');
+
+const clientSchema = new Schema({
+  owner: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+  name: { type: String, required: true },
+  type: { type: String, enum: ['Client', 'Lead', 'Vendor'], default: 'Client' },
+
+  phones: {
+    type: [String], // array of phone numbers
+    validate: [arr => arr.length <= 10, '{PATH} exceeds the limit of 10'],
+  },
+
+  emails: {
+    type: [String],
+    validate: [arr => arr.length <= 2, '{PATH} exceeds the limit of 2'],
+  },
+
+  company: { type: String },
+  notes: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const ClientModel = db.model('Client', clientSchema);
+
+module.exports = ClientModel;
---
+++ crm_backend/models/task_model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const db = require('../config/db');
+
+const taskSchema = new Schema({
+  owner: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+  title: { type: String, required: true },
+  description: { type: String },
+  status: { type: String, enum: ['Pending', 'Completed', 'In Process'], default: 'Pending' },
+  type: { type: String, enum: ['Call', 'Email', 'Deal', 'Meeting', 'None'], default: 'None' },
+
+  dueDate: { type: Date },
+  assignedTo: { type: Schema.Types.ObjectId, ref: 'user' },
+  relatedClient: { type: Schema.Types.ObjectId, ref: 'client' },
+
+  emails: {
+    type: [String],
+    validate: [arr => arr.length <= 2, '{PATH} exceeds the limit of 2'],
+  },
+
+  revenue: { type: Number },
+  cost: { type: Number },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const TaskModel = db.model('Task', taskSchema);
+module.exports = TaskModel;
