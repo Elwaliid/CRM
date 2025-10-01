e26be15b0219ef7cb9818bf83cd5434509565736
Elwaliid <walidboubaidja@gmail.com>
2025-10-01 21:36:56 +0100

    Update main.dart

--- crm_frontend/lib/main.dart
+++ crm_frontend/lib/main.dart
@@ -1,16 +1,16 @@
 // ignore_for_file: avoid_web_libraries_in_flutter, deprecated_member_use, prefer_typing_uninitialized_variables, use_super_parameters, duplicate_ignore
-// ignore: deprecated_member_use
+/* // ignore: deprecated_member_use
 import 'dart:html' as html;
-import 'package:crm_frontend/view/Screens/home_screen.dart';
+import 'package:crm_frontend/view/Screens/home_screen.dart'; */
 import 'package:crm_frontend/view/Screens/splash_screen.dart';
 import 'package:crm_frontend/ustils/google_signin_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/Get.dart';
-import 'package:jwt_decoder/jwt_decoder.dart';
+/* import 'package:jwt_decoder/jwt_decoder.dart'; */
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+/* import 'package:shared_preferences/shared_preferences.dart'; */
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
