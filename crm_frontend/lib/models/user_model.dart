2f7804945dbfc77a864d2f52d58669c055444df4
Elwaliid <walidboubaidja@gmail.com>
2025-10-08 19:07:21 +0100

    w

--- crm_frontend/lib/models/user_model.dart
+++ crm_frontend/lib/models/user_model.dart
@@ -1,10 +1,7 @@
 import 'dart:convert';
-import 'dart:ffi';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../ustils/config.dart';
 
 class UserModel {
